c8f5c036ecfd86b4e4f09f3f4eca74b951050fa4
VK <vitaliy.krynytskyy@gmail.com>
2019-10-31 11:27:38 +0000

    Added CreateLink component

---
+++ src/components/CreateLink/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class CreateLink extends React.Component {
+
+    state = {
+        description: '',
+        url: ''
+    };
+
+    render() {
+
+        const { description, url } = this.state;
+
+        return (
+            <div>
+                <div className="flex flex-column mt3">
+                    <input
+                        className="mb2"
+                        value={description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        type="text"
+                        placeholder="A description for the link"
+                    />
+
+                    <input
+                        className="mb2"
+                        value={url}
+                        onChange={e => this.setState({ url: e.target.value })}
+                        type="text"
+                        placeholder="The URL for the link"
+                    />
+
+                </div>
+                <button onClick={`do stuff`}>Submit</button>
+            </div>
+        );
+    }
+}
+
+export default CreateLink;
