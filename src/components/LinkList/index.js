2c36b97d23bab837e1f7bcad6f830badf908ac00
VK <vitaliy.krynytskyy@gmail.com>
2019-10-31 13:41:31 +0000

    Added subscriptions for votes and links

--- src/components/LinkList/index.js
+++ src/components/LinkList/index.js
@@ -27,6 +27,54 @@ export const FEED_QUERY = gql`
     }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+    subscription {
+        newVote {
+            id
+            link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+`;
+
 class LinkList extends React.Component {
 
     _updateCacheAfterVote = (store, createVote, linkId) => {
@@ -39,13 +87,44 @@ class LinkList extends React.Component {
         store.writeQuery({ query: FEED_QUERY, data });
     };
 
+    _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+
+                const newLink = subscriptionData.data.newLink;
+                const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename
+                    }
+                });
+            }
+        })
+    };
+
+    _subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_VOTES_SUBSCRIPTION
+        });
+    };
+
     render() {
 
         return (
             <Query query={FEED_QUERY}>
-                {({ loading, error, data }) => {
+                {({ loading, error, data, subscribeToMore }) => {
                     if (loading) return <div>Fetching results...</div>
                     if (error) return <div>Error Occured: {error}</div>
+
+                    this._subscribeToNewLinks(subscribeToMore);
+                    this._subscribeToNewVotes(subscribeToMore);
 
                     const linksToRender = data.feed.links;
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
 import { ApolloProvider } from 'react-apollo';
@@ -10,24 +8,52 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+import './styles/index.css';
+import App from './components/App'
 import { AUTH_TOKEN } from './constants';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000' // please use https://github.com/krynv/hacker-news-clone-with-prisma
 });
 
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+});
+
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem(AUTH_TOKEN)
+    const token = localStorage.getItem(AUTH_TOKEN);
+
     return {
         headers: {
             ...headers,
             authorization: token ? `Bearer ${token}` : ''
         }
-    }
+    };
 });
 
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+
+    wsLink,
+    authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 });
 
